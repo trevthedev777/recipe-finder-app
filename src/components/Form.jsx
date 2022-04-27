907455e18b5af8b568391d620aecc387bc468ec1
Your Name <you@example.com>
2022-04-27 20:17:52 +0200

    Initial commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+// Stateless Functional Component
+const Form = () => (
+    <form action="">
+        <input type="text" />
+        <button>Search</button>
+    </form>
+);
+
+export default Form;
